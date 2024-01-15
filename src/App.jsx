0d28a6830bd74a7ab4c4d5d91759e27c34ccbd6d
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-01-15 20:17:14 +0330

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,9 @@
+function SearchOrder() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchOrder
