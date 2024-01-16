441f05dc19c70a74d42b81ae81e5b405e681c18e
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-01-16 18:12:02 +0330

    dynamic cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -55,3 +55,5 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCarts = (state) => state.cart.cart;
