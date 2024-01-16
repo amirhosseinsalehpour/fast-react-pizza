68ced181069e090708e2886b15667c03a354503f
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-01-16 17:23:16 +0330

    Building the Cart Overview With Redux

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,8 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from './cartSlice';
 
 const fakeCart = [
   {
@@ -27,13 +29,20 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const dispatch = useDispatch();
+  const username = useSelector((state) => state.user.username);
+
+  function handkeClearCart() {
+    dispatch(clearCart());
+  }
+
   const cart = fakeCart;
 
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
@@ -46,7 +55,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={handkeClearCart}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
